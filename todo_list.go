6ba171a200b36e8b8f3ccc9df4262a75b31f8deb
Legion <legion26042010@gmail.com>
2025-12-03 19:53:52 +0500

    [v2] todo list project add recursion

--- todo_list.go
+++ todo_list.go
@@ -21,50 +21,51 @@ type task struct {
 func main() {
 	fmt.Println("Hello it's your ToDo app!")
 	taskList := make([]task, 0)
+	start(taskList)
+}
 
-	for {
-		fmt.Println("\n", "1. Add Task", "\n", "2. View Tasks", "\n", "3. Complete the task", "\n", "4. Delete Task", "\n", "5. Exit")
-		var input int
-		fmt.Scan(&input)
+func start(taskList []task) string {
+	fmt.Println("\n", "1. Add Task", "\n", "2. View Tasks", "\n", "3. Complete the task", "\n", "4. Delete Task", "\n", "5. Exit")
+	var input int
+	fmt.Scan(&input)
 
-		if input == 1 {
-			fmt.Println("Input task description:")
-			desc := Print()
-			date := "1999-12-31"
-			time.Parse(layoutISO, date)
-			newTask := task{description: desc, status: false, createdAt: date}
-			taskList = append(taskList, newTask)
-			fmt.Println("Task added!")
-		} else if input == 2 {
-			fmt.Println("Your Tasks:")
-			for i, t := range taskList {
-				fmt.Println(i+1, " Description:", t.description, "| Status: ", t.status, " | Created At: ", t.createdAt)
-			}
-		} else if input == 3 {
-			fmt.Println("Input task number to complete:")
-			var num int
-			fmt.Scanln(&num)
-			if num > 0 && num <= len(taskList) {
-				taskList[num-1].status = true
-				fmt.Println("Task marked as completed!")
-			} else {
-				fmt.Println("Task not complete! Enter valid task number.")
-			}
-		} else if input == 4 {
-			fmt.Println("Input task number to delete:")
-			var num int
-			fmt.Scanln(&num)
-			if num > 0 && num <= len(taskList) {
-				taskList = append(taskList[:num-1], taskList[num:]...)
-				fmt.Println("Task deleted!")
-			}
-		} else if input == 5 {
-			fmt.Println("Bye!")
-			break
-		} else {
-			fmt.Println("Invalid input")
+	if input == 1 {
+		fmt.Println("Input task description:")
+		desc := Print()
+		date := "1999-12-31"
+		time.Parse(layoutISO, date)
+		newTask := task{description: desc, status: false, createdAt: date}
+		taskList = append(taskList, newTask)
+		fmt.Println("Task added!")
+	} else if input == 2 {
+		fmt.Println("Your Tasks:")
+		for i, t := range taskList {
+			fmt.Println(i+1, " Description:", t.description, "| Status: ", t.status, " | Created At: ", t.createdAt)
 		}
+	} else if input == 3 {
+		fmt.Println("Input task number to complete:")
+		var num int
+		fmt.Scanln(&num)
+		if num > 0 && num <= len(taskList) {
+			taskList[num-1].status = true
+			fmt.Println("Task marked as completed!")
+		} else {
+			fmt.Println("Task not complete! Enter valid task number.")
+		}
+	} else if input == 4 {
+		fmt.Println("Input task number to delete:")
+		var num int
+		fmt.Scanln(&num)
+		if num > 0 && num <= len(taskList) {
+			taskList = append(taskList[:num-1], taskList[num:]...)
+			fmt.Println("Task deleted!")
+		}
+	} else if input == 5 {
+		return "Bye!"
+	} else {
+		fmt.Println("Invalid input")
 	}
+	return start(taskList)
 }
 
 func Print() string {
