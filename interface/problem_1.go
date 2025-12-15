444a216f82658357b8348debe9acee9cec39bdc9
Legion <legion26042010@gmail.com>
2025-12-16 01:24:25 +0500

    problem 1 by Miran about interface and io.Writer

---
+++ interface/problem_1.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"os"
+)
+
+type WriteWrapper struct {
+	write io.Writer
+}
+
+func (w *WriteWrapper) Write(p []byte) (n int, err error) {
+	return w.write.Write(p)
+}
+
+func Print(p io.Writer) {
+	p.Write([]byte("Hello, World!"))
+}
+
+func main() {
+	file, _ := os.Create("./problem.txt")
+	defer file.Close()
+	fileWriter := &WriteWrapper{write: file}
+	Print(fileWriter) // Записывает "Hello, World!" в файл problem.txt
+
+	consoleWriter := &WriteWrapper{write: os.Stdout}
+	Print(consoleWriter) // Выводит "Hello, World!" в консоль
+
+	var bufferWriter bytes.Buffer
+	bufferWrapper := &WriteWrapper{write: &bufferWriter}
+	Print(bufferWrapper) // Записывает "Hello, World!" в буфер
+	fmt.Println(bufferWriter.String())
+}
+
+/*
+ну реально же сложно пздец, там чёто с интерфейсами, он типо должен реализовать интерфейс io.writer
+крч понял немного как это работает, но не до конца
+а тут просто запысиаваю в файл без логера, там уже логер изучить придется
+*/
