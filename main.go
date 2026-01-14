44a9f6bc2a78e8b661c9754b0d642aa2a7fb30bb
Legion <legion26042010@gmail.com>
2026-01-14 19:15:15 +0500

    channel problems

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "golangCourse/gorutine/bankImitation"
+import (
+	"fmt"
+	"golangCourse/gorutine/channel"
+)
+
+var a int
 
 func main() {
-	bankImitation.Start()
+	go channel.Problem1()
+	for i := 1; i <= 5; i++ {
+		fmt.Println(<-channel.ChProblem1)
+	}
+
+	channel.Problem2()
+	for a := range channel.ChProblem2 {
+		fmt.Println(a)
+	}
+
 }
